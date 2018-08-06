04f071d0d72f4815759440bce276b61e28079752
LeratoThokolo <leratongoakothokolo@gmail.com>
2018-08-06 17:03:12 +0200

    Trading account controller exceptions

--- src/main/java/nedj/train/task1webservice/nedj/train/controller/TradingAccountController.java
+++ src/main/java/nedj/train/task1webservice/nedj/train/controller/TradingAccountController.java
@@ -2,6 +2,7 @@ package nedj.train.task1webservice.nedj.train.controller;
 
 
 import nedj.train.task1webservice.nedj.train.model.TradingAccount;
+import nedj.train.task1webservice.nedj.train.repository.TradingAccountRepository;
 import nedj.train.task1webservice.nedj.train.service.TradingAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +18,19 @@ public class TradingAccountController {
     private TradingAccountService tradingAccountService;
 
 
+    @Autowired
+    private TradingAccountRepository tradingAccountRepository;
+
+
+
     @RequestMapping(value = "create-account", method = RequestMethod.POST)
-    public TradingAccount createAccount(@RequestBody TradingAccount tradingAccount){
+    public TradingAccount createAccount(@RequestBody TradingAccount tradingAccount) throws Exception {
+
+        if(tradingAccount == null){
+
+            throw new Exception("Trading account is empty!!");
+
+        }
 
         return  this.tradingAccountService.createAccount(tradingAccount);
 
@@ -26,17 +38,27 @@ public class TradingAccountController {
 
 
     @RequestMapping(value = "/get-account-balance/{tradingAccountID}", method = RequestMethod.GET)
-    public double accountBalance(@PathVariable int tradingAccountID){
+    public double accountBalance(@PathVariable int tradingAccountID) throws Exception {
+
+        if(this.tradingAccountRepository.getOne(tradingAccountID) == null){
+
+            throw new Exception("No record for that identifier");
+        }
 
         return this.tradingAccountService.accountBalance(tradingAccountID);
     }
 
     @RequestMapping(value = "/accounts-list", method = RequestMethod.GET)
-    public List<TradingAccount> getTradingAccounts(){
+    public List<TradingAccount> getTradingAccounts() throws Exception {
+
+        if(this.tradingAccountService.getTradingAccounts().size() == 0){
+
+            throw new Exception("No trading accounts");
+
+        }
 
         return this.tradingAccountService.getTradingAccounts();
     }
-
 
 
 }
